a9f3e602a2ae0416ae100280d07ab938ae405937
Cristian Henrici <cristian.henrici@itdistrict.ch>
2021-12-03 16:25:15 +0100

    First commit

---
+++ src/components/TestVue.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>This message in microfrontend-second comes from app {{ msg }}</h1>
+</template>
+<script>
+export default {
+   name: 'TestVue',
+  props: {
+    msg: String
+  },
+}
+</script>
