f65a511f43960f487693f2a057cedea7502223ad
Cristian Henrici <cristian.henrici@itdistrict.ch>
2021-12-03 16:39:31 +0100

    added dist for microfrontend-approach

---
+++ dist/src_components_TestVue_vue.js
@@ -0,0 +1,62 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmicrofrontend_second"] = self["webpackChunkmicrofrontend_second"] || []).push([["src_components_TestVue_vue"],{
+
+/***/ "./src/components/TestVue.vue":
+/*!************************************!*\
+  !*** ./src/components/TestVue.vue ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _TestVue_vue_vue_type_template_id_a41798d8_bindings_msg_props___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TestVue.vue?vue&type=template&id=a41798d8&bindings={\"msg\":\"props\"} */ \"./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={\\\"msg\\\":\\\"props\\\"}\");\n/* harmony import */ var _TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TestVue.vue?vue&type=script&lang=js */ \"./src/components/TestVue.vue?vue&type=script&lang=js\");\n\n\n\n_TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"].render = _TestVue_vue_vue_type_template_id_a41798d8_bindings_msg_props___WEBPACK_IMPORTED_MODULE_0__.render\n/* hot reload */\nif (false) {}\n\n_TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"].__file = \"src/components/TestVue.vue\"\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (_TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"]);\n\n//# sourceURL=webpack://microfrontend-second/./src/components/TestVue.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=script&lang=js":
+/*!********************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=script&lang=js ***!
+  \********************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\r\n   name: 'TestVue',\r\n  props: {\r\n    msg: String\r\n  },\r\n});\r\n\n\n//# sourceURL=webpack://microfrontend-second/./src/components/TestVue.vue?./node_modules/vue-loader/dist/index.js??ruleSet%5B1%5D.rules%5B3%5D.use%5B0%5D");
+
+/***/ }),
+
+/***/ "./src/components/TestVue.vue?vue&type=script&lang=js":
+/*!************************************************************!*\
+  !*** ./src/components/TestVue.vue?vue&type=script&lang=js ***!
+  \************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* reexport safe */ _node_modules_vue_loader_dist_index_js_ruleSet_1_rules_3_use_0_TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]; }\n/* harmony export */ });\n/* harmony import */ var _node_modules_vue_loader_dist_index_js_ruleSet_1_rules_3_use_0_TestVue_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./TestVue.vue?vue&type=script&lang=js */ \"./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=script&lang=js\");\n \n\n//# sourceURL=webpack://microfrontend-second/./src/components/TestVue.vue?");
+
+/***/ }),
+
+/***/ "./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={\"msg\":\"props\"}":
+/*!*******************************************************************************************!*\
+  !*** ./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={"msg":"props"} ***!
+  \*******************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": function() { return /* reexport safe */ _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_1_node_modules_vue_loader_dist_index_js_ruleSet_1_rules_3_use_0_TestVue_vue_vue_type_template_id_a41798d8_bindings_msg_props___WEBPACK_IMPORTED_MODULE_0__.render; }\n/* harmony export */ });\n/* harmony import */ var _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_1_node_modules_vue_loader_dist_index_js_ruleSet_1_rules_3_use_0_TestVue_vue_vue_type_template_id_a41798d8_bindings_msg_props___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!../../node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./TestVue.vue?vue&type=template&id=a41798d8&bindings={\"msg\":\"props\"} */ \"./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={\\\"msg\\\":\\\"props\\\"}\");\n\n\n//# sourceURL=webpack://microfrontend-second/./src/components/TestVue.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={\"msg\":\"props\"}":
+/*!*************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[3].use[0]!./src/components/TestVue.vue?vue&type=template&id=a41798d8&bindings={"msg":"props"} ***!
+  \*************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": function() { return /* binding */ render; }\n/* harmony export */ });\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"webpack/sharing/consume/default/vue/vue\");\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(vue__WEBPACK_IMPORTED_MODULE_0__);\n\n\nfunction render(_ctx, _cache, $props, $setup, $data, $options) {\n  return ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(\"h1\", null, \"This message in microfrontend-second comes from app \" + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.msg), 1 /* TEXT */))\n}\n\n//# sourceURL=webpack://microfrontend-second/./src/components/TestVue.vue?./node_modules/vue-loader/dist/templateLoader.js??ruleSet%5B1%5D.rules%5B1%5D!./node_modules/vue-loader/dist/index.js??ruleSet%5B1%5D.rules%5B3%5D.use%5B0%5D");
+
+/***/ })
+
+}]);
